76cf4bbafaf2d94b002189216b1136144345f4b4
ramapge17 <38715223+ramapge17@users.noreply.github.com>
2021-12-03 20:27:09 +0530

    Modified items for 500 Internal Error

--- resources/items.py
+++ resources/items.py
@@ -70,7 +70,7 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items':[item.json for item in  ItemModel.query.all()]}
+        return {'items':[item.json() for item in  ItemModel.query.all()]}
         
 
         
